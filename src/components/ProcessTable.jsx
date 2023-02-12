e01d001c6c9527bdb8ae41278470aaca39ea9c3d
Denil Bhatt <denilbhatt2004@gmail.com>
2023-02-12 11:05:40 +0530

    integrated backend

--- src/components/ProcessTable.jsx
+++ src/components/ProcessTable.jsx
@@ -1,153 +1,141 @@
 // MUI
-import { RemoveCircleOutlineRounded } from '@mui/icons-material';
+import { RemoveCircleOutlineRounded } from "@mui/icons-material";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	TextField,
-	IconButton,
-	Paper
-} from '@mui/material';
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  IconButton,
+  Paper,
+} from "@mui/material";
 
-const ProcessTable = props => {
-	const { processes, setProcesses, errorText, setErrorText } = props;
+const ProcessTable = (props) => {
+  const { processes, setProcesses, errorText, setErrorText } = props;
 
-	const arrivalTimeUpdate = (event, index) => {
-		const t = processes;
-		t[index]['arrivalTime'] = event.target.value;
-		setProcesses(t);
-	};
+  const arrivalTimeUpdate = (event, index) => {
+    const t = processes;
+    t[index]["arrivalTime"] = event.target.value;
+    setProcesses(t);
+  };
 
-	const burstTimeUpdate = (event, index) => {
-		const t = processes;
-		t[index]['burstTime'] = event.target.value;
-		setProcesses(t);
-	};
+  const burstTimeUpdate = (event, index) => {
+    const t = processes;
+    t[index]["burstTime"] = event.target.value;
+    setProcesses(t);
+  };
 
-	const deleteProcess = (event, index) => {
-		const e = [
-			...errorText.slice(0, index),
-			...errorText.slice(index + 1, errorText.length)
-		];
+  const deleteProcess = (event, index) => {
+    const e = [
+      ...errorText.slice(0, index),
+      ...errorText.slice(index + 1, errorText.length),
+    ];
 
-		const t = [
-			...processes.slice(0, index),
-			...processes.slice(index + 1, processes.length)
-		];
+    const t = [
+      ...processes.slice(0, index),
+      ...processes.slice(index + 1, processes.length),
+    ];
 
-		setErrorText(e);
-		setProcesses(t);
-	};
+    setErrorText(e);
+    setProcesses(t);
+  };
 
-	return (
-		<TableContainer component={Paper}>
-			<Table
-				sx={{
-					backdropFilter: 'blur(15px)',
-					borderRadius: '12px'
-				}}
-			>
-				{/* Table Header */}
-				<TableHead>
-					<TableRow>
-						<TableCell align='center'>PID</TableCell>
+  return (
+    <TableContainer component={Paper}>
+      <Table
+        sx={{
+          backdropFilter: "blur(15px)",
+          borderRadius: "12px",
+        }}
+      >
+        {/* Table Header */}
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">PID</TableCell>
 
-						<TableCell align='center'>Arrival Time (AT)</TableCell>
-						<TableCell align='center'>Burst Time (BT)</TableCell>
+            <TableCell align="center">Arrival Time (AT)</TableCell>
+            <TableCell align="center">Burst Time (BT)</TableCell>
 
-						<TableCell align='center'>
-							Completion Time (CT)
-						</TableCell>
+            <TableCell align="center">Completion Time (CT)</TableCell>
 
-						<TableCell align='center'>
-							Turn around time (CT - AT)
-						</TableCell>
+            <TableCell align="center">Turn around time (CT - AT)</TableCell>
 
-						<TableCell align='center'>Waiting Time</TableCell>
+            <TableCell align="center">Waiting Time</TableCell>
 
-						<TableCell align='center'>ResponseTime</TableCell>
+            <TableCell align="center">ResponseTime</TableCell>
 
-						<TableCell align='center' />
-					</TableRow>
-				</TableHead>
+            <TableCell align="center" />
+          </TableRow>
+        </TableHead>
 
-				{/* Table Body */}
-				<TableBody>
-					{processes.map((process, i) => (
-						<TableRow
-							key={process.pid}
-							sx={{
-								'&:last-child td, &:last-child th': {
-									border: 0
-								}
-							}}
-						>
-							{/* PID Cell */}
-							<TableCell>{process.pid}</TableCell>
+        {/* Table Body */}
+        <TableBody>
+          {processes.map((process, i) => (
+            <TableRow
+              key={process.pid}
+              sx={{
+                "&:last-child td, &:last-child th": {
+                  border: 0,
+                },
+              }}
+            >
+              {/* PID Cell */}
+              <TableCell>{process.pid}</TableCell>
 
-							{/* Arrival Time Input */}
-							<TableCell align='center'>
-								<TextField
-									size='small'
-									defaultValue={process.arrivalTime}
-									onChange={e => arrivalTimeUpdate(e, i)}
-									inputProps={{
-										style: { textAlign: 'center' }
-									}}
-									error={errorText[i][0] !== ''}
-									helperText={errorText[i][0]}
-								/>
-							</TableCell>
+              {/* Arrival Time Input */}
+              <TableCell align="center">
+                <TextField
+                  size="small"
+                  defaultValue={process.arrivalTime}
+                  onChange={(e) => arrivalTimeUpdate(e, i)}
+                  inputProps={{
+                    style: { textAlign: "center" },
+                  }}
+                  error={errorText[i][0] !== ""}
+                  helperText={errorText[i][0]}
+                />
+              </TableCell>
 
-							{/* Burst Time Input */}
-							<TableCell align='center'>
-								<TextField
-									size='small'
-									defaultValue={process.burstTime}
-									onChange={e => burstTimeUpdate(e, i)}
-									inputProps={{
-										style: { textAlign: 'center' }
-									}}
-									error={errorText[i][1] !== ''}
-									helperText={errorText[i][1]}
-								/>
-							</TableCell>
+              {/* Burst Time Input */}
+              <TableCell align="center">
+                <TextField
+                  size="small"
+                  defaultValue={process.burstTime}
+                  onChange={(e) => burstTimeUpdate(e, i)}
+                  inputProps={{
+                    style: { textAlign: "center" },
+                  }}
+                  error={errorText[i][1] !== ""}
+                  helperText={errorText[i][1]}
+                />
+              </TableCell>
 
-							{/* Completion Time Cell */}
-							<TableCell align='center'>
-								{process.completionTime}
-							</TableCell>
+              {/* Completion Time Cell */}
+              <TableCell align="center">{process.completionTime}</TableCell>
 
-							{/* Turnaround Time Cell */}
-							<TableCell align='center'>
-								{process.turnAroundTime}
-							</TableCell>
+              {/* Turnaround Time Cell */}
+              <TableCell align="center">{process.turnAroundTime}</TableCell>
 
-							{/* Waiting Time Cell */}
-							<TableCell align='center'>
-								{process.waitingTime}
-							</TableCell>
+              {/* Waiting Time Cell */}
+              <TableCell align="center">{process.waitingTime}</TableCell>
 
-							{/* Response Time Cell */}
-							<TableCell align='center'>
-								{process.completionTime}
-							</TableCell>
+              {/* Response Time Cell */}
+              <TableCell align="center">{process.responseTime}</TableCell>
 
-							{/* Delete Row Button */}
-							<TableCell align='center'>
-								<IconButton onClick={e => deleteProcess(e, i)}>
-									<RemoveCircleOutlineRounded color='error' />
-								</IconButton>
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
+              {/* Delete Row Button */}
+              <TableCell align="center">
+                <IconButton onClick={(e) => deleteProcess(e, i)}>
+                  <RemoveCircleOutlineRounded color="error" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default ProcessTable;
